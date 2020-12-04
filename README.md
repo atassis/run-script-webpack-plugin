0ac2e0ff35cff376502197d9d4603a08e1a49180
Taymuraz Kaytmazov <atassikay38@gmail.com>
2020-12-05 01:43:40 +0300

    feat: initial commit with working prototype

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # run-script-webpack-plugin
+
+> Automatically start your server once Webpack's build completes.
+ 
+NOTE: mostly copied from [this](https://github.com/ericclemmons/start-server-webpack-plugin) repo, but strongly typed from scratch
+
+[![version](https://img.shields.io/npm/v/run-script-webpack-plugin.svg)](http://npm.im/run-script-webpack-plugin)
+[![downloads](https://img.shields.io/npm/dm/run-script-webpack-plugin.svg)](http://npm-stat.com/charts.html?package=run-script-webpack-plugin)
+[![MIT License](https://img.shields.io/npm/l/run-script-webpack-plugin.svg)](http://opensource.org/licenses/MIT)
+
+### Installation
+
+```shell
+$ npm i -D run-script-webpack-plugin
+```
+
+### Usage
+
+In `webpack.config.js`:
+
+```js
+import { RunScriptWebpackPlugin } from "run-script-webpack-plugin";
+
+export default {
+  plugins: [
+    ...
+    // Only use this in DEVELOPMENT
+    new RunScriptWebpackPlugin({
+      name: 'server.js',
+      nodeArgs: ['--inspect'], // allow debugging
+      args: ['scriptArgument1', 'scriptArgument2'], // pass args to script
+      signal: false | true | 'SIGUSR2', // signal to send for HMR (defaults to `false`, uses 'SIGUSR2' if `true`)
+      keyboard: true | false, // Allow typing 'rs' to restart the server. default: only if NODE_ENV is 'development'
+    }),
+  ],
+}
+```
+
+The `name` argument in `RunScriptWebpackPluginOptions` refers to the built asset, which is named by the output options of webpack (in the example the entry `server` becomes `server.js`. This way, the plugin knows which entry to start in case there are several.
+
+If you don't pass a name, the plugin will tell you the available names.
+
+You can use `nodeArgs` and `args` to pass arguments to node and your script, respectively. For example, you can use this to use the node debugger.
+
+To use Hot Module Reloading with your server code, set Webpack to "hot" mode and include the `webpack/hot/poll` or `webpack/hot/signal` modules. Make sure they are part of your server bundle, e.g. if you are using `node-externals` put them in your whitelist. The latter module requires the `signal` option.
+
+### License
+
+> Refer to [LICENSE](LICENSE) file
---
+++ src/index.ts
@@ -0,0 +1,186 @@
+import cluster, { ClusterSettings, Worker } from 'cluster';
+import webpack, { Compiler, WebpackPluginInstance } from 'webpack';
+import Compilation = webpack.compilation.Compilation;
+
+export type ProcessKillSignal =
+  | 'SIGHUP'
+  | 'SIGINT'
+  | 'SIGQUIT'
+  | 'SIGILL'
+  | 'SIGABRT'
+  | 'SIGFPE'
+  | 'SIGKILL'
+  | 'SIGSEGV'
+  | 'SIGPIPE'
+  | 'SIGALRM'
+  | 'SIGTERM'
+  | 'SIGUSR1'
+  | 'SIGUSR2'
+  | 'SIGCHLD'
+  | 'SIGCONT'
+  | 'SIGSTOP'
+  | 'SIGTSTP'
+  | 'SIGTTIN'
+  | 'SIGTTOU'
+  | 'SIGBUS'
+  | 'SIGPOLL'
+  | 'SIGPROF'
+  | 'SIGSYS'
+  | 'SIGTRAP'
+  | 'SIGURG'
+  | 'SIGVTALRM'
+  | 'SIGXCPU'
+  | 'SIGXFSZ'
+  | 'SIGIOT'
+  | 'SIGEMT'
+  | 'SIGSTKFLT'
+  | 'SIGIO'
+  | 'SIGCLD'
+  | 'SIGPWR'
+  | 'SIGINFO'
+  | 'SIGLOST'
+  | 'SIGWINCH'
+  | 'SIGUNUSED';
+
+export type RunScriptWebpackPluginOptions = {
+  name?: string;
+  nodeArgs: string[];
+  args: string[];
+  signal: boolean | ProcessKillSignal;
+  keyboard: boolean;
+  restartable?: boolean;
+};
+
+function getInspectPort(execArgv: string[]): number | void {
+  const inspectArg = execArgv.find((arg) => arg.includes('--inspect'));
+  if (!inspectArg || !inspectArg.includes('=')) {
+    return;
+  }
+  const hostPort = inspectArg.split('=')[1];
+  const port = hostPort.includes(':') ? hostPort.split(':')[1] : hostPort;
+  return parseInt(port);
+}
+
+function getSignal(signal: ProcessKillSignal | boolean) {
+  // allow users to disable sending a signal by setting to `false`...
+  if (signal === false) return;
+  if (signal === true) return 'SIGUSR2';
+  return signal;
+}
+
+class RunScriptWebpackPlugin implements WebpackPluginInstance {
+  private readonly options: RunScriptWebpackPluginOptions;
+
+  private worker?: Worker;
+
+  private _entrypoint?: string;
+
+  constructor(options: Partial<RunScriptWebpackPluginOptions> = {}) {
+    this.options = {
+      signal: false,
+      // Only listen on keyboard in development, so the server doesn't hang forever
+      keyboard: process.env.NODE_ENV === 'development',
+      ...options,
+      args: [...(options.args || [])],
+      nodeArgs: [...process.execArgv, ...(options.nodeArgs || [])],
+    };
+
+    if (this.options.restartable) {
+      this._enableRestarting();
+    }
+  }
+
+  private _enableRestarting(): void {
+    if (this.options.keyboard) {
+      process.stdin.setEncoding('utf8');
+      process.stdin.on('data', (data: string) => {
+        if (data.trim() === 'rs') {
+          console.log('Restarting app...');
+          if (this.worker) {
+            process.kill(this.worker.process.pid);
+          }
+          this._startServer((worker) => {
+            this.worker = worker;
+          });
+        }
+      });
+    }
+  }
+
+  private afterEmit = (compilation: Compilation, cb: () => void): void => {
+    if (this.worker && this.worker.isConnected()) {
+      const signal = getSignal(this.options.signal);
+      if (signal) {
+        process.kill(this.worker.process.pid, signal);
+      }
+      cb();
+      return;
+    }
+
+    this.startServer(compilation, cb);
+  }
+
+  apply = (compiler: Compiler): void => {
+    compiler.hooks.afterEmit.tapAsync(
+      { name: 'RunScriptPlugin' },
+      this.afterEmit,
+    );
+  }
+
+  private startServer = (compilation: Compilation, cb: () => void): void => {
+    const { assets, compiler } = compilation;
+    const { options } = this;
+    let name;
+    const names = Object.keys(assets);
+    if (options.name) {
+      name = options.name;
+      if (!assets[name]) {
+        console.error(
+          `Entry ${name} not found. Try one of: ${names.join(' ')}`,
+        );
+      }
+    } else {
+      name = names[0];
+      if (names.length > 1) {
+        console.log(
+          `More than one entry built, selected ${name}. All names: ${names.join(
+            ' ',
+          )}`,
+        );
+      }
+    }
+    if (!compiler.options.output || !compiler.options.output.path) {
+      throw new Error('output.path should be defined in webpack config!');
+    }
+
+    this._entrypoint = `${compiler.options.output.path}/${name}`;
+    this._startServer((worker) => {
+      this.worker = worker;
+      cb();
+    });
+  }
+
+  private _startServer(cb: (arg0: Worker) => void): void {
+    const { args, nodeArgs } = this.options;
+    const inspectPort = getInspectPort(nodeArgs);
+
+    const clusterOptions: ClusterSettings = {
+      exec: this._entrypoint,
+      execArgv: nodeArgs,
+      args,
+    };
+
+    if (inspectPort) {
+      clusterOptions.inspectPort = inspectPort;
+    }
+    cluster.setupMaster(clusterOptions);
+
+    cluster.on('online', (worker) => {
+      cb(worker);
+    });
+
+    cluster.fork();
+  }
+}
+
+export { RunScriptWebpackPlugin };
